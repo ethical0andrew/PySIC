36685568a7ba60a3731f592d7a728466d822565a
Andrew <85117454+ethical0andrew@users.noreply.github.com>
2022-02-01 09:42:17 +0200

    SRC 1.1 - More Browsers
    SRC 1.1 - Added more Browser Support including Google Chrome, Microsoft Edge, Mozilla Firefox & Brave.

--- src/pysic.py
+++ src/pysic.py
@@ -4,12 +4,30 @@ import time
 import pyautogui
 import keyboard
 working_dir = os.path.abspath(os.getcwd())
+with open('pysic_browser.txt') as inputfile:
+    browser_name = inputfile.read()
+if (browser_name == ""):
+    print("Browser set to Google Chrome")
+    print("browser_pysic.txt file is empty")
+    browser_dir = "C:\Program Files\Google\Chrome\Application && chrome.exe file:///"
+if (browser_name == "chrome"):
+    print("Browser set to Google Chrome")
+    browser_dir = "C:\Program Files\Google\Chrome\Application && chrome.exe file:///"
+if (browser_name == "edge"):
+    print("Browser set to Microsoft Edge")
+    browser_dir = "C:\Program Files (x86)\Microsoft\Edge\Application && msedge.exe file:///"
+if (browser_name == "firefox"):
+    print("Browser set to Mozilla Firefox")
+    browser_dir = "C:\Program Files\Mozilla Firefox && firefox.exe file:///"
+if (browser_name == "brave"):
+    print("Browser set to Brave")
+    browser_dir = "C:\Program Files\BraveSoftware\Brave-Browser\Application && brave.exe file:///"
 html_filename = str(sys.argv[1])
 if ".html" not in html_filename:
     print("Failed: File type not compatible")
     exit()
 else:
-    os.system('cd C:\Program Files\Google\Chrome\Application && chrome.exe file:///' + working_dir + "\\" + html_filename)
+    os.system('cd ' + browser_dir + working_dir + "\\" + html_filename)
 while True:
     if (keyboard.is_pressed("ctrl+s")):
         pyautogui.keyDown('alt')
